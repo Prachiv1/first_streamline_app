b30f673fdcf3241c33b421a0dfc30cacde93ee27
Prachiv1 <134619383+Prachiv1@users.noreply.github.com>
2023-06-12 17:00:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,6 @@ streamlit.dataframe(fruits_to_show)
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response.json())
 # Allows to pick some fruits
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # displays selected fruits
